75aaa6531fc7ca861cb0425fb3f7fd47c14d73fa
Lucas Otaño <lucasmatiasota@gmail.com>
2023-10-27 15:14:30 -0300

    docs: corregir la doc

--- division.js
+++ division.js
@@ -1,7 +1,7 @@
 /**
- * Division function. Recibe 2 valores y los divide
+ * Division function. Recibe 2 valores y retorna la división
  * @param {number} a
  * @param {number} b
  * @returns {number}
  */
-const division = (a, b) => a/ b;
+const division = (a, b) => a / b;
